57b0ced65ef5d1f257f477e4da640199b71ce907
Jaydbrown <jaiyeolawety705@gmail.com>
2025-09-26 01:22:33 -0400

    server.js

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ const allowedOrigins = [
   'http://localhost:5500',
   'https://bid-bit.netlify.app',
   'https://bid-it-frontend-t5wk.vercel.app',
-  'https://bid-it-frontend-t5wk-far1tzqwp-jaydbrowns-projects.vercel.app'
+  'https://bid-it-frontend-t5wk-far1tzqwp-jaydbrowns-projects.vercel.app',
+  'https://bid-it.com.ng'
 ];
 
 // Helmet with relaxed CSP for images + frontend communication
@@ -108,6 +109,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} on port ${PORT}`)
 );
+
 
 
 
